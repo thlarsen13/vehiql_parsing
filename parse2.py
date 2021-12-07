d43608d128f687a8929957fc9aecbf32a3f360f7
thlarsen13 <thomas.larsen031@gmail.com>
2021-12-07 11:15:07 -0500

    assign types to nodes for easier processing downstream in pipeline

--- parse2.py
+++ parse2.py
@@ -1,23 +1,73 @@
 from pythonds.basic import Stack
-# from pythonds.trees import BinaryTree
 
 
-# s = '( ( RIGHT(argument1, argument2) AND (ego.something=left_turn OR traffic_light.CONFIDENCE=0.9 ) ) OR t.COLOR=red )'
 """
 Query Language Rules: 
-There are three syntactic objects: words, parenthesis, and spaces. words & parenthesis must be seperated by parentheses. 
-We allow n-ary operators, (AND, OR) and atomic statements. 
+There are two syntactic "words": functions and parenthesis. Anything not seperated by spaces will be treated as its own "word".  
+We create a tree, with leaf nodes being functions, and everything else being AND/OR, according to SQL order of operations. 
 
 """
 
+#like a Binary Tree but nodes can have multiple children
 class ChristmasTree:
     def __init__(self, root): 
-        self.key = root
-        self.node_type = "" #TYPES: COMP, FUNCTION, 
 
+        self.logical_operators = ["AND", "OR"]
+        self.function_symbols = ["LEFT", "RIGHT", "BEFORE", "AFTER"]
+        self.comp_operators = ["<", ">", "="]
+
+        self.key = root
+        self.node_type = "" #TYPES: COMP, FUNCTION. TODO: keep track of this. 
         self.children = []
 
-    def insertChild(self,newNode):
+        #only used for comparison type 
+        self.var = None #"ego"
+        self.attr = None #"speed"
+        self.comp_operator = None # < 
+        self.target = None # 30 (mph)
+
+        #only used for function types
+        self.function = self.isFunctionSym()
+        self.args = None
+
+
+        if self.key in self.logical_operators:
+            self.node_type = "LOGICAL_OPERATOR"
+        elif self.function is not None: 
+            self.node_type = "FUNCTION"
+            L = len(self.function)
+            rest = self.key[L+1:-1]
+            self.args = rest.split(",")
+
+        elif "." in self.key: 
+            self.node_type = "COMPARISON"
+
+            self.var = self.key.split(".")[0]
+            rest = self.key.split(".")[1]
+
+            found = False
+            for op in self.comp_operators: 
+                if op in rest: 
+                    if found:
+                        print("oops: multiple comp ops found")
+                        print("exit")
+                    i = rest.index(op)
+                    self.attr = rest[:i]            
+                    self.comp_operator = op
+                    self.target = rest[i+1:]
+                    found = True 
+        else: 
+            print(f"ERROR, {self.key} not recognized")
+            exit()
+
+    def isFunctionSym(self): 
+        for sym in self.function_symbols: 
+            if self.key[:len(sym)] == sym: 
+                return sym 
+        return None
+
+
+    def insertChild(self,newNode): 
 
         if isinstance(newNode, ChristmasTree):
             t = newNode
@@ -28,12 +78,18 @@ class ChristmasTree:
 
 
     def print_tree(self, depth = 0):
-        print("   "*depth + "+---" + self.key)
+        verbose = False 
+        if depth == 0: 
+            print(self.key)
+        else: 
+            print("     "*(depth-1) + "+--- " + self.key)
+        if verbose and self.node_type == "COMPARISON": 
+            print("     "*(depth) + f"var: {self.var}, attr: {self.attr}, op: {self.comp_operator}, target: {self.target}")
+        if verbose and self.node_type == "FUNCTION": 
+            print("     "*(depth) + f"function: {self.function}, args: {self.args}")
         for child in self.children:
             child.print_tree(depth=depth+1)
 
-
-binary_operators = ["AND", "OR"]
 
 
 def find_close(query, i): 
@@ -52,12 +108,8 @@ def find_close(query, i):
     return j - 1
 
 
-class treeMaker(): 
+class ChristmasTreeFarmer(): 
     def __init__(self, query ): 
-        # pStack = Stack()
-        # eTree = ChristmasTree('')
-        # pStack.push(eTree)
-        # currentTree = eTree
         self.priority = {
         "AND" : 1,
         "OR" : 0
@@ -66,13 +118,15 @@ class treeMaker():
         self.treeStack = Stack()
         self.query = query
 
+        self.logical_operators = ["AND", "OR"]
+
+
     def makeTree(self):
         i = 0
         while i < len(self.query):
             word = self.query[i] 
-            print(word)
-            
-            if word in binary_operators: 
+
+            if word in self.logical_operators: 
                 if (self.operStack.size() > 0) and self.priority[self.operStack.peek()] > self.priority[word]: 
                 #we have a tighter op, so we put all of those into a tree 
                     self.pop_op() 
@@ -86,11 +140,12 @@ class treeMaker():
                     print(i, j)
                     print("unmatched (, AHHHH")
                     exit() 
-                tM = treeMaker(self.query[i + 1 : j]) 
+                tM = ChristmasTreeFarmer(self.query[i + 1 : j]) 
                 newTree = tM.makeTree()
                 # recurse into parenthesis, make tree, add it to the stack
                 self.treeStack.push(newTree)
                 i = j 
+                #we recursively processed i->j, so skip ahead to the rest of the string
 
             else: #insert a new word 
                 self.treeStack.push(ChristmasTree(word))
@@ -100,7 +155,9 @@ class treeMaker():
             self.pop_op() 
         ### check operStack is empty, and treeStack has only one tree 
         t = self.treeStack.pop() 
-        assert self.treeStack.size() == 0
+        if self.treeStack.size() != 0: 
+            print("Error: operations have mismatched number of args (or I have a bug)")
+            exit()
         return t 
 
     def pop_op(self): 
@@ -110,20 +167,14 @@ class treeMaker():
 
         c1 = self.treeStack.pop() 
         newTree.children.append(c1)
-        # print("\n---c1---")
-        # c1.print_tree()
 
         while (self.operStack.size() > 0) and self.operStack.peek() == op: 
           self.operStack.pop()
           ci = self.treeStack.pop()
           newTree.children.append(ci)
-          # print("\n---ci---")
-          # ci.print_tree()
 
         c2 = self.treeStack.pop() 
         newTree.children.append(c2)
-        # print("---c2---")
-        # c2.print_tree()
 
         self.treeStack.push(newTree)
 
@@ -133,12 +184,14 @@ def sentenceToTree(query):
     # query = resolve_syntax(query)
     # print(query)
     # pt = buildParseTree(query)
-    tM = treeMaker(query)
+    tM = ChristmasTreeFarmer(query)
     pt = tM.makeTree()
-    print("\n FINAL TREE \n")
+    print("\nFINAL TREE\n")
     pt.print_tree() 
+
 # q = '( ( ( RIGHTargument1,argument2 ) AND ( ego.something=left_turn OR traffic_light.CONFIDENCE=0.9 ) AND arg1 ) OR t.COLOR=red )'
-# q = "1 AND 2 AND 2.5 OR 3 AND 4"
-q = '( ego.something=left_turn OR traffic_light.CONFIDENCE=0.9 ) AND ( arg1 OR t.COLOR=red ) OR 1 OR 3'
+# q = "1 AND 2 AND 2.5 OR 3 AND 4 OR 5"
+q = 'taylor.hair=curly OR ( ego.something=left_turn OR traffic_light.CONFIDENCE=0.9 ) AND ( t.COLOR=green OR t.COLOR=red ) OR RIGHT(ego,traffic_light)'
+print('Query parsed: ' + q)
 
 sentenceToTree(q)
